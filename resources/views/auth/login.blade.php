ae1f0f94a1c08b47dd9ec618047f423dcf1b8cff
lynkxyz <lynkxyz@gmail.com>
2016-12-18 10:20:47 +0700

    update login style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,32 +7,12 @@
 
 <link href="{{ url('public/assets/bootstrap.min.css') }}" rel="stylesheet">
 <link href="{{ url('public/assets/styles.css') }}" rel="stylesheet">
-<link href="{{ url('public/css/customs.css') }}" rel="stylesheet">
-<link href="{{ url('public/assets/bootstrap-select.min.css') }}" rel="stylesheet">
-<link href="{{ url('public/assets/tokenfield-typeahead.min.css') }}" rel="stylesheet">
-<link href="{{ url('public/assets/bootstrap-tokenfield.min.css') }}" rel="stylesheet">
-
-<!--Icons-->
-<script src="{{ url('public/js/lumino.glyphs.js') }}"></script>
 
 <!--[if lt IE 9]>
 <script src="js/html5shiv.js"></script>
 <script src="js/respond.min.js"></script>
 <![endif]-->
 
-<!--angular 2-->
-<!--<script src="{{ url('public/core-js/client/shim.min.js') }}"></script>
-<script src="{{ url('public/zone.js/dist/zone.js') }}"></script>
-<script src="{{ url('public/reflect-metadata/Reflect.js') }}"></script>
-<script src="{{ url('public/systemjs/dist/system.src.js') }}"></script>
-<script src="{{ url('systemjs.config.js') }}"></script>
-
-<script>
-	System.import('app').catch(function(err) { 
-		console.error(err);
-	});
-</script>-->
-<!--end angular 2-->
 
 </head>
 
@@ -72,14 +52,12 @@
 	</div>
 	
 
-	<script src="{{ url('public/js/jquery-1.11.1.min.js') }}"></script>
-	<script src="{{ url('public/js/bootstrap.min.js') }}"></script>
-	<script src="{{ url('public/js/chart.min.js') }}"></script>
-	<script src="{{ url('public/js/bootstrap-select.min.js') }}"></script>
-	<script src="{{ url('public/js/bootstrap-tokenfield.min.js') }}"></script>
-	<script src="{{ url('public/js/typeahead.bundle.min.js') }}"></script>
-	<script src="{{ url('public/js/jquery.uploadPreview.min.js') }}"></script>
-	<script src="{{ url('public/js/myscript.js') }}"></script>
+	<!-- jQuery-->
+	<script src="{{ url('public/components/jquery/dist/jquery.min.js')}}"></script>
+	<!-- tether-->
+	<script src="{{ url('public/components/tether/dist/js/tether.min.js')}}"></script>
+	<!-- bootstrap-->
+	<script src="{{ url('public/components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
 </body>
 
 </html>
