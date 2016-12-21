8ab9946eb7d8c2159dd4bf0913c03e117e16164b
lynkxyz <lynkxyz@gmail.com>
2016-12-21 10:38:07 +0700

    sidebar member

--- resources/views/member/component/left-sidebar.blade.php
+++ resources/views/member/component/left-sidebar.blade.php
@@ -1,5 +1,6 @@
 <div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
     <ul class="nav menu">
+        <li><a href="{!! route('myaccount.index') !!}">{{$currentUser['username']}}</a></li>
         <li><a href="{!! route('home.index') !!}"><svg class="glyph stroked dashboard-dial"><use xlink:href="#stroked-dashboard-dial"></use></svg> Home</a></li>
         <li><a href="{!! route('myaccount.index') !!}"><svg class="glyph stroked male user"><use xlink:href="#stroked-male-user"/></svg> My Account</a></li>
     </ul>
